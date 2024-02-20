1aeb21bb08d0b18fed200289c36fbb24a46e7dd9
calebsheridan <calebsheridan@users.noreply.github.com>
2024-02-20 08:30:58 -0500

    Update deployments

--- lib/Deployments.tsx
+++ lib/Deployments.tsx
@@ -1,8 +1,8 @@
 import { parseAbiItem } from "viem"
 
 export const executionNodeAdd: `0x${string}` = '0x03493869959c866713c33669ca118e774a30a0e5'
-export const suaveContractAddress: `0x${string}` = "0xbA1B456C993B5c18DBCC25aBEA7FcCE081EF57a8"
-export const suaveDeployBlock: bigint = BigInt(1355423)
+export const suaveContractAddress: `0x${string}` = "0x0829fC66B8D714f789e3f333c10d38B6B92D0D70"
+export const suaveDeployBlock: bigint = BigInt(2115019)
 
 export const EventRequestAdded = parseAbiItem('event RequestAdded(uint256 indexed id, string extra, uint256 blockLimit)')
 export const EventRequestIncluded = parseAbiItem('event RequestIncluded(uint256 indexed id, uint64 egp, string blockHashLimit)')
