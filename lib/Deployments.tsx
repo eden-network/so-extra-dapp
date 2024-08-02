1a483ad58104c9e1e3d9986d5cb3c7da9994cec4
calebsheridan <calebsheridan@users.noreply.github.com>
2024-08-02 12:33:56 +0100

    Update deployment addresses for toliman

--- lib/Deployments.tsx
+++ lib/Deployments.tsx
@@ -1,8 +1,14 @@
-import { parseAbiItem } from "viem"
+import { Address, parseAbiItem } from "viem"
 
-export const executionNodeAdd: `0x${string}` = '0x03493869959c866713c33669ca118e774a30a0e5'
-export const suaveContractAddress: `0x${string}` = "0xE7C6324E56343a4FFD1DE56Fe2a0eB7D5A768dbB"
-export const suaveDeployBlock: bigint = BigInt(3253347)
+type SupportedSuaveChainIds = 33626250 | 16813125
+
+export const executionNodes: Record<SupportedSuaveChainIds, Address> = {
+    [33626250]: '0xf579de142d98f8379c54105ac944fe133b7a17fe',
+    [16813125]: '0x03493869959c866713c33669ca118e774a30a0e5'
+}
+
+export const suaveContractAddress: `0x${string}` = "0xa60F1B5cB70c0523A086BbCbe132C8679085ea0E"
+export const suaveDeployBlock: bigint = BigInt(423895)
 
 export const EventRequestAdded = parseAbiItem('event RequestAdded(uint256 indexed id, string extra, uint256 blockLimit)')
 export const EventRequestIncluded = parseAbiItem('event RequestIncluded(uint256 indexed id, uint64 egp, string blockHashLimit)')
