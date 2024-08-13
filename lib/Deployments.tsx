3d38ac3c8fed69b83a4c11c812e1840e5e62d661
calebsheridan <calebsheridan@users.noreply.github.com>
2024-08-13 09:42:58 +0200

    Update contract address

--- lib/Deployments.tsx
+++ lib/Deployments.tsx
@@ -7,8 +7,8 @@ export const executionNodes: Record<SupportedSuaveChainIds, Address> = {
     [16813125]: '0x03493869959c866713c33669ca118e774a30a0e5'
 }
 
-export const suaveContractAddress: `0x${string}` = "0xdCF5bAc09AC802d166c4EF44B8F828602D20E321"
-export const suaveDeployBlock: bigint = BigInt(831584)
+export const suaveContractAddress: `0x${string}` = "0xe0f06e942c06b558B8d0C58cA5fEf1A114516c02"
+export const suaveDeployBlock: bigint = BigInt(969472)
 
 export const EventRequestAdded = parseAbiItem('event RequestAdded(uint256 indexed id, string extra, uint256 blockLimit)')
 export const EventRequestIncluded = parseAbiItem('event RequestIncluded(uint256 indexed id, uint64 egp, string blockHashLimit)')
